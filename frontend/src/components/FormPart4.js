69b3b01a8acff02abb4d633a1ea1022a59b2c03c
Yash7426 <lcs2022057@iiitl.ac.in>
2023-05-30 22:16:45 +0530

    State Management and Routing

--- frontend/src/components/FormPart4.js
+++ frontend/src/components/FormPart4.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-function FormPart4() {
+function FormPart4({formD,setFormD}) {
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setFormD(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
     return (
         <>
             <table style={{ width: "100%" }}>
@@ -12,27 +22,27 @@ function FormPart4() {
                         <td style={{ width: "100%", display: "flex", flexDirection: "column" }} className='input_label_gapper' >
                             <div>
                                 <label htmlFor='reason1' style={{ padding: "0.5em" }} >a.{"  "}Lack of awareness of bank processes requirements</label>
-                                <input type="checkbox" id="reason1" name="Key_Reason_Preventing_loan" value="Lack of awareness of bank processes requirements" />
+                                <input type="checkbox" id="reason1" name="Key_Reason_Preventing_loan" value="Lack of awareness of bank processes requirements" checked={formD.Key_Reason_Preventing_loan=="Lack of awareness of bank processes requirements"} onChange={handleChange}/>
                             </div>
                             <div>
                                 <label htmlFor='reason2' style={{ padding: "0.5em" }} >b.{"  "}High Interest Rate </label>
-                                <input type="checkbox" id="reason2" name="Key_Reason_Preventing_loan" value="High Interest Rate " />
+                                <input type="checkbox" id="reason2" name="Key_Reason_Preventing_loan" value="High Interest Rate" checked={formD.Key_Reason_Preventing_loan=="High Interest Rate"} onChange={handleChange} />
                             </div>
                             <div>
                                 <label htmlFor='reason3' style={{ padding: "0.5em" }} >c.{"  "}Longer Processing time </label>
-                                <input type="checkbox" id="reason3" name="Key_Reason_Preventing_loan" value="Longer Processing time " />
+                                <input type="checkbox" id="reason3" name="Key_Reason_Preventing_loan" value="Longer Processing time" checked={formD.Key_Reason_Preventing_loan=="Longer Processing time"} onChange={handleChange}/>
                             </div>
                             <div>
                                 <label htmlFor='reason4' style={{ padding: "0.5em" }} >d.{"  "} Improper documentation </label>
-                                <input type="checkbox" id="reason4" name="Key_Reason_Preventing_loan" value="Improper documentation " />
+                                <input type="checkbox" id="reason4" name="Key_Reason_Preventing_loan" value="Improper documentation" checked={formD.Key_Reason_Preventing_loan=="Improper documentation"} onChange={handleChange}/>
                             </div>
                             <div>
                                 <label htmlFor='reason5' style={{ padding: "0.5em" }} >e.{"  "} No nearby bank branches  </label>
-                                <input type="checkbox" id="reason5" name="Key_Reason_Preventing_loan" value="No nearby bank branches " />
+                                <input type="checkbox" id="reason5" name="Key_Reason_Preventing_loan" value="No nearby bank branches" checked={formD.Key_Reason_Preventing_loan=="No nearby bank branches"} onChange={handleChange}/>
                             </div>
                             <div style={{ display: "flex", flexWrap: "wrap" }}>
                                 <label htmlFor='reason6' style={{ padding: "0.5em" }} >f.{"  "} Others, Please specify ... </label>
-                                <input type="text" id="reason6" name="Key_Reason_Preventing_loan" />
+                                <input type="text" id="reason6" name="Key_Reason_Preventing_loan" value={formD.Key_Reason_Preventing_loan} onChange={handleChange}/>
                             </div>
                         </td>
                     </tr>
@@ -44,7 +54,7 @@ function FormPart4() {
                             </label>
                         </td>
                         <td colSpan={5}>
-                            <input type='text' id='secQuestion' name='source_of_credit_form' />
+                            <input type='text' id='secQuestion' name='source_of_credit_form' onChange={handleChange} value={formD.source_of_credit_form}/>
                         </td>
                     </tr>
                     <tr>
@@ -91,6 +101,8 @@ function FormPart4() {
                 </thead>
 
             </table>
+            <NavLink to="/3">Prev</NavLink>
+        <NavLink to="/5">Next</NavLink>
         </>
     )
 }
