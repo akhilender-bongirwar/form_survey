6c2fcf7acf1a7c957175f27d26481550dc192fc9
umeshSinghVerma <usv0308@gmail.com>
2023-06-03 05:54:09 +0530

    checking the links

--- backend/frontend/src/components/FormPart6.js
+++ backend/frontend/src/components/FormPart6.js
@@ -17,7 +17,7 @@ function FormPart6({ formD, setFormD }) {
     if (!isV) {
     } else {
       axios
-        .post("https://form-survey-hhvh05mu4-umeshsinghverma.vercel.app/form-data", {
+        .post("https://form-survey-34a5hvu2o-umeshsinghverma.vercel.app/form-data", {
           mode: "cors",
           body: formD,
         })
