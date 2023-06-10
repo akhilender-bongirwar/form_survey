609ebe0979b8f2a41b1d11ab08371fb3578acb44
umeshSinghVerma <usv0308@gmail.com>
2023-06-10 23:52:08 +0530

    msmi logo update

--- backend/frontend/src/components/Frontpart.js
+++ backend/frontend/src/components/Frontpart.js
@@ -11,7 +11,7 @@ export default function Frontpart() {
           menubar.classList.toggle('expand');
         }} tabIndex="1" >MENU</button>
       </div>
-      
+
       <div className="menubar">
         <button className='arrow_notation' onClick={(e) => {
           e.preventDefault();
@@ -19,22 +19,29 @@ export default function Frontpart() {
           menubar.classList.toggle('expand');
         }} tabIndex="1">Close</button>
         <div className="menubarcontent">
-        <button className='btns'><NavLink to="/" style={{color:"white",textDecoration:"none"}}>Home Page </NavLink></button>
-        <button className='btns'>Project Dashboard</button>
+          <button className='btns'><NavLink to="/" style={{ color: "white", textDecoration: "none" }}>Home Page </NavLink></button>
+          <button className='btns'>Project Dashboard</button>
         </div>
       </div>
 
       <div className="hero">
         <div className="imges">
-          <img src="/msmelogo.png" alt="" />
-          <img src="https://iiitl.ac.in/wp-content/uploads/2019/10/Final_Logo_IIITL.png" alt="" />
+          {/* <img src="/msmelogo.png" alt="" /> */}
+          <div className='msme'>
+            <img class="national_emblem" src="https://msme.gov.in/sites/all/themes/msme/images/emblem-dark.png" alt="national emblem" />
+            <div>
+              <p lang="hi">सूक्ष्म , लघु और मध्यम उद्यम मंत्रालय</p>
+              <p class="maintitle1">Ministry of <br /> <strong>Micro, small &amp; Medium Enterprises </strong></p>
+            </div>
+          </div>
+          <img className='iiitl' src="https://iiitl.ac.in/wp-content/uploads/2019/10/Final_Logo_IIITL.png" alt="" />
         </div>
-        <p style={{ fontWeight: "700" , fontSize:"xx-large" }}>RAMP Project</p>
+        <p style={{ fontWeight: "700", fontSize: "xx-large" }}>RAMP Project</p>
         <p>RAMP Project App for Managing all Operations</p>
       </div>
       <div className="buttonclass">
-        <button className='btns'><NavLink to="/uploadSurvey" style={{color:"white",textDecoration:"none"}}>Upload Survey Documents </NavLink></button>
-        <button className='btns'><NavLink to="/eForm"  style={{color:"white",textDecoration:"none"}}>RAMP E-Survey Form </NavLink></button>
+        <button className='btns'><NavLink to="/uploadSurvey" style={{ color: "white", textDecoration: "none" }}>Upload Survey Documents </NavLink></button>
+        <button className='btns'><NavLink to="/eForm" style={{ color: "white", textDecoration: "none" }}>RAMP E-Survey Form </NavLink></button>
       </div>
     </div>
   )
